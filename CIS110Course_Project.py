7a7bc5a867cfb482627a865b578c7330ac85a10d
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-02-07 06:54:25 -0800

    Add project files.

---
+++ CIS110Course_Project.py
@@ -0,0 +1,42 @@
+print("Greetings, Welcome to The Lost Puppy story.")
+print("This story will detail an experience a lost puppy encountered while trying to make their way back home.")
+print("To personalize this story, please answer the next few questions. Please press enter after each answer.")
+input("Press the enter key to continue...")
+
+print("*************")
+userName = input("What is your name?:   ")
+print("*************")
+
+dogBreed = input("\nWhat breed is the lost dog?: ")
+puppyName = input("\nWhat is the puppy name?: ")
+capitalName = input("\nWhat is the capital of your state?: ")
+parksName = input("\nWhat is the name of a dog park near you?:  ")
+favoriteFood = input("\nWhat is your favoirte food?:  ")
+
+print("*************")
+print("So, " +userName+  " Are you ready to begin?  ")
+print("**************")
+
+
+print("\nThe lost puppy was a "  +dogBreed+  " named "  +puppyName+ "  who encounters Danny the Doberman Pincher")
+print("before he passes  " +parksName+ ". " +puppyName+ "  decides to take another route....")
+print("bypassing the " +parksName+ "  dog park and goes over the bridge instead.")
+print("By taking this route,  " +puppyName+ " the " +dogBreed+ " avoids any conflict with Danny.")
+print("As  " +puppyName+ "  crosses the bridge they pick up on a familiar scent.....")
+
+
+print("\n**************")
+print("As,  " +puppyName+ "  the  " +dogBreed+  " continues over the bridge in,  " +parksName+ " they notice the scent")
+print("of a " +favoriteFood+ " food stand.")
+print("However,..." +puppyName+ "  is a little hesitant as they remeber that " +capitalName+ " has a strict rule")
+print("against " +dogBreed+ " that are not accompained by an owner.")
+print("Just as " +puppyName+ " began to weep, when they heard a familiar voice of thier owner...")
+print("However, it was just thier imagination. " +puppyName+ " decides to keep searching..." )
+print("\n***************")
+
+
+print("After going down to " +capitalName+ " City Hall, where thier owner works.")
+print("To no avail, " +puppyName+ " the " +dogBreed+ " decides to see if they can make their way back home before the sunsets.")
+print("As,  " +puppyName+ " is halfway through the city of " +capitalName+ " they remember")
+print("that " +parksName+ " Animal Control will be able to locate thier owner with thier puppytag.")
+print("Anmial Control at " +parksName+ " was able call the owner and " +puppyName+ " is estatic!")
